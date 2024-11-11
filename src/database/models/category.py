2a4eea76a6f89ce9eb1c620329627d4ed5936afb
Tahsin Rashad <tahsin.rashad716@gmail.com>
2024-11-11 12:54:55 +0000

    category model added

---
+++ src/database/models/category.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+
+from src.database.models.base import Base
+
+
+class Category(Base):
+    __tablename__ = 'categories'
+
+    category_id = Column(Integer, primary_key=True)
+    name = Column(String(50), unique=True, nullable=False)
+    description = Column(String)
+
+    expenses = relationship("Expense", back_populates="category")
