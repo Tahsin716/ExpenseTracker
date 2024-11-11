7ab8b8b360ed5bea5a35cb2d98272502bb518143
Tahsin Rashad <tahsin.rashad716@gmail.com>
2024-11-11 12:56:31 +0000

    expense model added

---
+++ src/database/models/expense.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+from sqlalchemy import Column, Integer, ForeignKey, Float, String, DateTime
+from sqlalchemy.orm import relationship
+
+from src.database.models.base import Base
+
+
+class Expense(Base):
+    __tablename__ = 'expenses'
+
+    expense_id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.user_id'), nullable=False)
+    category_id = Column(Integer, ForeignKey('categories.category_id'), nullable=False)
+    amount = Column(Float, nullable=False)
+    description = Column(String)
+    date = Column(DateTime, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+    user = relationship("User", back_populates="expenses")
+    category = relationship("Category", back_populates="expenses")
