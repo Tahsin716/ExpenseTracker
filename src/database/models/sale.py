4b2ac3f94d2da387b14e49a6d9de6af47670dc52
Tahsin Rashad <tahsin.rashad716@gmail.com>
2024-11-11 13:06:55 +0000

    sale model added

---
+++ src/database/models/sale.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+from sqlalchemy import Integer, Column, ForeignKey, Float, DateTime
+from sqlalchemy.orm import relationship
+
+from src.database.models.base import Base
+
+
+class Sale(Base):
+    __tablename__ = 'sales'
+
+    sale_id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.user_id'), nullable=False)
+    total_amount = Column(Float, nullable=False)
+    sale_date = Column(DateTime, default=datetime.utcnow)
+
+    user = relationship("User", back_populates="sales")
+    sale_items = relationship("SaleItem", back_populates="sale")
