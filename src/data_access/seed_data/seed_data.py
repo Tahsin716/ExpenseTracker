ded0c05487d30093cb9a77df4c637379b2ab303d
Tahsin Rashad <Tahsin716@users.noreply.github.com>
2024-12-05 14:34:53 +0000

    Update seed_data.py

--- src/data_access/seed_data/seed_data.py
+++ src/data_access/seed_data/seed_data.py
@@ -1,7 +1,5 @@
 from datetime import datetime, timezone
-
-from sqlalchemy import select
-
+from sqlalchemy import select, func
 from src.business.providers.roles import Roles
 from src.business.providers.security_context import SecurityContext
 from src.business.utils.password_manager import PasswordManager
@@ -38,164 +36,138 @@ class SeedData(DataAccess):
             self.session.close()
 
     def expense_seed(self):
-        expenses = [
-            Expense(
-                category_id=1,
-                amount=300.0,
-                description="Rent for last month",
-                date=datetime(2024, 11, 2, tzinfo=timezone.utc)
-            ),
-            Expense(
-                category_id=2,
-                amount=500.0,
-                description="Utility bill for last month",
-                date=datetime(2024, 11, 6, tzinfo=timezone.utc)
-            )
-        ]
-        for expense in expenses:
-            exists = self.session.execute(
-                select(Expense).where(
-                    (Expense.category_id == expense.category_id) &
-                    (Expense.date == expense.date)
+        if not self.session.execute(select(func.count(Expense.expense_id))).scalar():
+            expenses = [
+                Expense(
+                    category_id=1,
+                    amount=300.0,
+                    description="Rent for last month",
+                    date=datetime(2024, 11, 2, tzinfo=timezone.utc)
+                ),
+                Expense(
+                    category_id=2,
+                    amount=500.0,
+                    description="Utility bill for last month",
+                    date=datetime(2024, 11, 6, tzinfo=timezone.utc)
                 )
-            ).scalar_one_or_none()
-            if not exists:
+            ]
+            for expense in expenses:
                 self.session.add(expense)
 
     def sale_item_seed(self):
-        sale_items = [
-            SaleItem(
-                sale_id=1,
-                inventory_item_id=1,
-                quantity=2,
-                price_per_unit=35.0  # Espresso Beans
-            ),
-            SaleItem(
-                sale_id=2,
-                inventory_item_id=2,
-                quantity=1,
-                price_per_unit=2.0  # Milk
-            ),
-            SaleItem(
-                sale_id=3,
-                inventory_item_id=5,
-                quantity=5,
-                price_per_unit=3.50  # Pastries
-            ),
-            SaleItem(
-                sale_id=4,
-                inventory_item_id=6,
-                quantity=10,
-                price_per_unit=1.50  # Tea Bags
-            )
-        ]
-        for sale_item in sale_items:
-            exists = self.session.execute(
-                select(SaleItem).where(
-                    (SaleItem.sale_id == sale_item.sale_id) &
-                    (SaleItem.inventory_item_id == sale_item.inventory_item_id)
+        if not self.session.execute(select(func.count(SaleItem.sale_item_id))).scalar():
+            sale_items = [
+                SaleItem(
+                    sale_id=1,
+                    inventory_item_id=1,
+                    quantity=2,
+                    price_per_unit=35.0  # Espresso Beans
+                ),
+                SaleItem(
+                    sale_id=2,
+                    inventory_item_id=2,
+                    quantity=1,
+                    price_per_unit=2.0  # Milk
+                ),
+                SaleItem(
+                    sale_id=3,
+                    inventory_item_id=5,
+                    quantity=5,
+                    price_per_unit=3.50  # Pastries
+                ),
+                SaleItem(
+                    sale_id=4,
+                    inventory_item_id=6,
+                    quantity=10,
+                    price_per_unit=1.50  # Tea Bags
                 )
-            ).scalar_one_or_none()
-            if not exists:
+            ]
+            for sale_item in sale_items:
                 self.session.add(sale_item)
 
     def sale_seed(self):
-        sales = [
-            Sale(
-                total_amount=70.0,  # 2*35.0
-                sale_date=datetime(2024, 11, 1, tzinfo=timezone.utc),
-                customer_id=1
-            ),
-            Sale(
-                total_amount=2.0,  # 1*2.0
-                sale_date=datetime(2024, 11, 5, tzinfo=timezone.utc),
-                customer_id=2
-            ),
-            Sale(
-                total_amount=17.50,  # 5*3.50
-                sale_date=datetime(2024, 11, 10, tzinfo=timezone.utc),
-                customer_id=3
-            ),
-            Sale(
-                total_amount=15.0,  # 10*1.50
-                sale_date=datetime(2024, 11, 15, tzinfo=timezone.utc),
-                customer_id=4
-            )
-        ]
-
-        for sale in sales:
-            exists = self.session.execute(
-                select(Sale).where(Sale.sale_date == sale.sale_date)
-            ).scalar_one_or_none()
-            if not exists:
+        if not self.session.execute(select(func.count(Sale.sale_id))).scalar():
+            sales = [
+                Sale(
+                    total_amount=70.0,  # 2*35.0
+                    sale_date=datetime(2024, 11, 1, tzinfo=timezone.utc),
+                    customer_id=1
+                ),
+                Sale(
+                    total_amount=2.0,  # 1*2.0
+                    sale_date=datetime(2024, 11, 5, tzinfo=timezone.utc),
+                    customer_id=2
+                ),
+                Sale(
+                    total_amount=17.50,  # 5*3.50
+                    sale_date=datetime(2024, 11, 10, tzinfo=timezone.utc),
+                    customer_id=3
+                ),
+                Sale(
+                    total_amount=15.0,  # 10*1.50
+                    sale_date=datetime(2024, 11, 15, tzinfo=timezone.utc),
+                    customer_id=4
+                )
+            ]
+            for sale in sales:
                 self.session.add(sale)
 
     def user_seed(self):
-        users = [
-            User(first_name="John", last_name="Doe",
-                 password_hash=PasswordManager.hash_password(SecurityContext.default_password),
-                 email="john.doe@example.com", role=Roles.ADMIN),
-            User(first_name="Jane", last_name="Smith",
-                 password_hash=PasswordManager.hash_password(SecurityContext.default_password),
-                 email="jane.smith@example.com", role=Roles.USER),
-            User(first_name="Alice", last_name="Johnson",
-                 password_hash=PasswordManager.hash_password(SecurityContext.default_password),
-                 email="alice.johnson@example.com", role=Roles.USER),
-            User(first_name="Bob", last_name="Brown",
-                 password_hash=PasswordManager.hash_password(SecurityContext.default_password),
-                 email="bob.brown@example.com", role=Roles.USER),
-            User(first_name="Charlie", last_name="Davis",
-                 password_hash=PasswordManager.hash_password(SecurityContext.default_password),
-                 email="charlie.davis@example.com", role=Roles.ADMIN)
-        ]
-
-        for user in users:
-            exists = self.session.execute(select(User).where(User.email == user.email)).scalar_one_or_none()
-            if not exists:
+        if not self.session.execute(select(func.count(User.user_id))).scalar():
+            users = [
+                User(first_name="John", last_name="Doe",
+                     password_hash=PasswordManager.hash_password(SecurityContext.default_password),
+                     email="john.doe@example.com", role=Roles.ADMIN),
+                User(first_name="Jane", last_name="Smith",
+                     password_hash=PasswordManager.hash_password(SecurityContext.default_password),
+                     email="jane.smith@example.com", role=Roles.USER),
+                User(first_name="Alice", last_name="Johnson",
+                     password_hash=PasswordManager.hash_password(SecurityContext.default_password),
+                     email="alice.johnson@example.com", role=Roles.USER),
+                User(first_name="Bob", last_name="Brown",
+                     password_hash=PasswordManager.hash_password(SecurityContext.default_password),
+                     email="bob.brown@example.com", role=Roles.USER),
+                User(first_name="Charlie", last_name="Davis",
+                     password_hash=PasswordManager.hash_password(SecurityContext.default_password),
+                     email="charlie.davis@example.com", role=Roles.ADMIN)
+            ]
+            for user in users:
                 self.session.add(user)
 
     def inventory_seed(self):
-        inventory_items = [
-            InventoryItem(name="Espresso Beans", description="High-quality espresso beans", quantity=50, cost_price=20, selling_price=35),
-            InventoryItem(name="Milk", description="Fresh whole milk", quantity=200, cost_price=1, selling_price=2),
-            InventoryItem(name="Sugar", description="Refined sugar packets", quantity=500, cost_price=0.10, selling_price=0.50),
-            InventoryItem(name="Coffee Cups", description="12 oz coffee cups", quantity=1000, cost_price=0.20, selling_price=1),
-            InventoryItem(name="Pastries", description="Assorted fresh pastries", quantity=100, cost_price=1.50, selling_price=3.50),
-            InventoryItem(name="Tea Bags", description="Various tea blends", quantity=300, cost_price=0.25, selling_price=1.50)
-        ]
-
-        for item in inventory_items:
-            exists = self.session.execute(select(InventoryItem).where(InventoryItem.name == item.name)).scalar_one_or_none()
-            if not exists:
+        if not self.session.execute(select(func.count(InventoryItem.item_id))).scalar():
+            inventory_items = [
+                InventoryItem(name="Espresso Beans", description="High-quality espresso beans", quantity=50, cost_price=20, selling_price=35),
+                InventoryItem(name="Milk", description="Fresh whole milk", quantity=200, cost_price=1, selling_price=2),
+                InventoryItem(name="Sugar", description="Refined sugar packets", quantity=500, cost_price=0.10, selling_price=0.50),
+                InventoryItem(name="Coffee Cups", description="12 oz coffee cups", quantity=1000, cost_price=0.20, selling_price=1),
+                InventoryItem(name="Pastries", description="Assorted fresh pastries", quantity=100, cost_price=1.50, selling_price=3.50),
+                InventoryItem(name="Tea Bags", description="Various tea blends", quantity=300, cost_price=0.25, selling_price=1.50)
+            ]
+            for item in inventory_items:
                 self.session.add(item)
 
     def customer_seed(self):
-        customers = [
-            Customer(phone_number="+447912345678"),
-            Customer(phone_number="+447876543210"),
-            Customer(phone_number="+447911112222"),
-            Customer(phone_number="+447812345678"),
-            Customer(phone_number="+447833333444")
-        ]
-
-        for customer in customers:
-            exists = self.session.execute(
-                select(Customer).where(Customer.phone_number == customer.phone_number)
-            ).scalar_one_or_none()
-            if not exists:
+        if not self.session.execute(select(func.count(Customer.customer_id))).scalar():
+            customers = [
+                Customer(phone_number="+447912345678"),
+                Customer(phone_number="+447876543210"),
+                Customer(phone_number="+447911112222"),
+                Customer(phone_number="+447812345678"),
+                Customer(phone_number="+447833333444")
+            ]
+            for customer in customers:
                 self.session.add(customer)
 
     def category_seed(self):
-        categories = [
-            Category(name="Rent", description="Monthly rent for the coffee shop premises"),
-            Category(name="Utilities", description="Expenses for electricity, water, and other utilities"),
-            Category(name="Payroll", description="Salaries and wages for employees"),
-            Category(name="Supplies", description="Coffee beans, cups, and other necessary supplies"),
-            Category(name="Equipment Maintenance", description="Costs for maintaining and repairing equipment"),
-            Category(name="Marketing", description="Advertising and promotional expenses")
-        ]
-
-        for category in categories:
-            exists = self.session.execute(select(Category).where(Category.name == category.name)).scalar_one_or_none()
-            if not exists:
+        if not self.session.execute(select(func.count(Category.category_id))).scalar():
+            categories = [
+                Category(name="Rent", description="Monthly rent for the coffee shop premises"),
+                Category(name="Utilities", description="Expenses for electricity, water, and other utilities"),
+                Category(name="Payroll", description="Salaries and wages for employees"),
+                Category(name="Supplies", description="Coffee beans, cups, and other necessary supplies"),
+                Category(name="Equipment Maintenance", description="Costs for maintaining and repairing equipment"),
+                Category(name="Marketing", description="Advertising and promotional expenses")
+            ]
+            for category in categories:
                 self.session.add(category)
